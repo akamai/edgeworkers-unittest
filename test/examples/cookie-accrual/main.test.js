c8a956581d4ed5bdd4357893308ad50261790e88
ahsankamal <ahsankamal60@gmail.com>
2021-06-22 12:27:38 -0300

    unit tests for remaining EW examples

---
+++ test/examples/cookie-accrual/main.test.js
@@ -0,0 +1,56 @@
+import {onClientResponse} from "../../../src/examples/cookie-accrual/main";
+import Request from "../../../__mocks__/request";
+import Response from "../../../__mocks__/response";
+import {SetCookie, mock_Cookies_get, mock_SetCookie_toHeader} from "../../../__mocks__/cookies";
+
+
+describe('Accrue values in a cookie as a visitor traverses a site, and trigger a promotional cookie.', () => {
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+  
+    test("onClientResponse should set visited cookie in response header when no. of sections visited is zero", () => {
+        let requestMock = new Request();
+        requestMock.path = "/cookieaccrual/about-us/";
+        let responseMock = new Response();
+        mock_SetCookie_toHeader.mockReturnValue("visited cookie: about-us");
+
+        onClientResponse(requestMock, responseMock);
+        expect(SetCookie).toHaveBeenCalledTimes(1);
+        expect(SetCookie).toHaveBeenCalledWith({"maxAge": 86400, "name": "visited", "path": "/", "value": ",about-us"});
+        expect(responseMock.setHeader).toHaveBeenCalledTimes(1);
+        expect(responseMock.setHeader).toHaveBeenCalledWith("Set-Cookie", ["visited cookie: about-us"]);   
+    });
+
+    test("onClientResponse should set visited cookie in response header when no. of sections visited is within max limit", () => {
+        let requestMock = new Request();
+        requestMock.path = "/cookie-accural/history/";
+        let responseMock = new Response();
+        mock_Cookies_get.mockReturnValue("about-us");
+        mock_SetCookie_toHeader.mockReturnValue("visited cookie: about-us,history");
+
+        onClientResponse(requestMock, responseMock);
+        expect(SetCookie).toHaveBeenCalledTimes(1);
+        expect(SetCookie).toHaveBeenCalledWith({"maxAge": 86400, "name": "visited", "path": "/", "value": "about-us,history"});
+        expect(responseMock.setHeader).toHaveBeenCalledTimes(1);
+        expect(responseMock.setHeader).toHaveBeenCalledWith("Set-Cookie", ["visited cookie: about-us,history"]);   
+    });
+
+    test("onClientResponse should set visited and promo cookie in response header when no. of sections visited exceeds max limit", () => {
+        let requestMock = new Request();
+        requestMock.path = "/cookie-accural/sizing/";
+        let responseMock = new Response();
+        mock_Cookies_get.mockReturnValue("about-us,history,products,guarantee,pricing");
+        mock_SetCookie_toHeader.mockReturnValueOnce("visited cookie: about-us,history,products,guarantee,pricing,sizing").mockReturnValue("promo cookie: true");
+
+        onClientResponse(requestMock, responseMock);
+        expect(SetCookie).toHaveBeenCalledTimes(2);
+        expect(SetCookie).toHaveBeenCalledWith({"maxAge": 86400, "name": "visited", "path": "/", "value": "about-us,history,products,guarantee,pricing,sizing"});
+        expect(SetCookie).toHaveBeenCalledWith({ "name": "promo", "value": "true", "path": "/", "maxAge": 900 });
+        expect(responseMock.setHeader).toHaveBeenCalledTimes(1);
+        expect(responseMock.setHeader).toHaveBeenCalledWith("Set-Cookie", ["visited cookie: about-us,history,products,guarantee,pricing,sizing", "promo cookie: true"]);   
+    });
+
+});
+
--- test/examples/origin-overload/main.test.js
+++ test/examples/origin-overload/main.test.js
@@ -17,6 +17,7 @@ test("onOriginResponse should invoke respondWith if response.status is 503", () 
     expect(responseMock.addHeader).toHaveBeenCalledTimes(1);
     expect(responseMock.addHeader).toHaveBeenCalledWith('Origin-Response-Status', responseMock.status);
     expect(requestMock.respondWith).toHaveBeenCalledTimes(1);
+    expect(requestMock.respondWith).toHaveBeenCalledWith(200, { 'Content-Type': ['text/html'] }, '<html><script> setTimeout(function () { window.location.href="' + escape(requestMock.path) + '"; }, ' + 10 + '*1000);</script> <body>The origin server is currently overloaded, please retry in ' + 10 + ' seconds </body></html>');
   });
 
 test("onOriginResponse should not invoke respondWith if response.status is not 503", () => {
