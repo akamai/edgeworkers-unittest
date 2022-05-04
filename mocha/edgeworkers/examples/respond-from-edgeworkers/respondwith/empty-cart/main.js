80e8a52f99926607d118af362f2da73822333f18
Aditya Tingare <atingare@akamai.com>
2022-05-04 11:36:05 -0400

    Removed comments on new test EdgeWorkers

--- mocha/edgeworkers/examples/respond-from-edgeworkers/respondwith/empty-cart/main.js
+++ mocha/edgeworkers/examples/respond-from-edgeworkers/respondwith/empty-cart/main.js
@@ -1,12 +1,4 @@
-/*
-(c) Copyright 2019 Akamai Technologies, Inc. Licensed under Apache 2 license.
-
-Version: 0.1
-Purpose:  Respond with empty JSON if cart cookie not part of request.
-Repo: https://github.com/akamai/edgeworkers-examples/tree/master/empty-cart
-*/
-
-import { Cookies, SetCookie } from 'cookies';
+import { Cookies } from 'cookies';
 
 export function onClientResponse (request, response) {
   const cookies = new Cookies(request.getHeader('Cookie'));
