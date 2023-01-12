6c137ee2f617f15a0068482d8b9fad5c3dcb3212
Tanishq Bhatia <mrtanishqbhatia@gmail.com>
2023-01-12 15:52:10 -0500

    [EW-11966]: Test framework update for respondWith in onOrigin*()

---
+++ jest/test/examples/index-of-respond-with/main.test.js
@@ -0,0 +1,41 @@
+import Request from "request";
+import Response from "response";
+import {
+  onOriginRequest,
+  onOriginResponse
+} from "../../../src/edgeworkers/examples/respond-from-edgeworkers/respondwith/index-of/main";
+
+describe("EdgeWorker that will respond with a JSON based on query passed", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("return JSON when `respondWithonOriginRequestTest` is present in query parameters", () => {
+    const requestMock = new Request();
+    requestMock.query = "respondWithonOriginRequestTest=hello";
+
+    onOriginRequest(requestMock);
+    expect(requestMock.respondWith).toHaveBeenCalledTimes(1);
+    expect(requestMock.respondWith).toHaveBeenCalledWith(
+      200,
+      // eslint-disable-next-line camelcase
+      { respond_with_stage: "onOriginRequest" },
+      "response constructed by ew\r\n"
+    );
+  });
+
+  test("return JSON when `respondWithonOriginResponseTest` is present in query parameters", () => {
+    const requestMock = new Request();
+    const responseMock = new Response();
+    requestMock.query = "respondWithonOriginResponseTest=hi";
+
+    onOriginResponse(requestMock, responseMock);
+    expect(requestMock.respondWith).toHaveBeenCalledTimes(1);
+    expect(requestMock.respondWith).toHaveBeenCalledWith(
+      200,
+      // eslint-disable-next-line camelcase
+      { respond_with_stage: "onOriginResponse" },
+      "response constructed by ew\r\n"
+    );
+  });
+});
