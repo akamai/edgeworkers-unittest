681cd17f52373df49b37e4a4396b84f0de2ba957
Tanishq Bhatia <mrtanishqbhatia@gmail.com>
2023-03-22 09:27:10 -0400

    [EW-15963]: Test framework update for HTML Rewriter

--- jest/__mocks__/html-rewriter.js
+++ jest/__mocks__/html-rewriter.js
@@ -1,8 +1,36 @@
 export const mockHtmlRewritingStream = jest.fn();
-export const mockOnElement = jest.fn();
+export const mockOnElement = jest.fn().mockImplementation(() => {
+  return {
+    selector: String,
+    handler: onElement()
+  };
+});
 export const mockReadableStream = jest.fn().mockReturnThis();
 export const mockWritableStream = jest.fn().mockReturnThis();
 export const mockWritableStreamPipeThrough = jest.fn().mockReturnThis();
+export const mockAfter = jest.fn();
+export const mockAppend = jest.fn();
+export const mockBefore = jest.fn();
+export const mockGetAttribute = jest.fn();
+export const mockPrepend = jest.fn();
+export const mockRemoveAttribute = jest.fn();
+export const mockReplaceChildren = jest.fn();
+export const mockReplaceWith = jest.fn();
+export const mockSetAttribute = jest.fn();
+
+export const onElement = jest.fn().mockImplementation(() => {
+  return {
+    after: mockAfter,
+    append: mockAppend,
+    before: mockBefore,
+    getAttribute: mockGetAttribute,
+    prepend: mockPrepend,
+    removeAttribute: mockRemoveAttribute,
+    replaceChildren: mockReplaceChildren,
+    replaceWith: mockReplaceWith,
+    setAttribute: mockSetAttribute
+  };
+});
 
 export const HtmlRewritingStream = mockHtmlRewritingStream.mockImplementation(
   () => {
--- mocha/__mocks__/html-rewriter.js
+++ mocha/__mocks__/html-rewriter.js
@@ -1,7 +1,12 @@
 const sinon = require("sinon");
 
 export const mockHtmlRewritingStream = sinon.stub();
-export const mockOnElement = sinon.stub();
+export const mockOnElement = sinon.stub().callsFake(() => {
+  return {
+    selector: String,
+    handler: onElement()
+  };
+});
 export const mockReadableStream = sinon.stub().callsFake(() => {
   return {};
 });
@@ -11,6 +16,28 @@ export const mockWritableStream = sinon.stub().callsFake(() => {
 export const mockWritableStreamPipeThrough = sinon.stub().callsFake(() => {
   return {};
 });
+
+export const mockAfter = sinon.stub();
+export const mockAppend = sinon.stub();
+export const mockBefore = sinon.stub();
+export const mockGetAttribute = sinon.stub();
+export const mockPrepend = sinon.stub();
+export const mockRemoveAttribute = sinon.stub();
+export const mockReplaceChildren = sinon.stub();
+export const mockReplaceWith = sinon.stub();
+export const mockSetAttribute = sinon.stub();
+
+const onElement = () => {
+  this.after = mockAfter;
+  this.append = mockAppend;
+  this.before = mockBefore;
+  this.getAttribute = mockGetAttribute;
+  this.prepend = mockPrepend;
+  this.removeAttribute = mockRemoveAttribute;
+  this.replaceChildren = mockReplaceChildren;
+  this.replaceWith = mockReplaceWith;
+  this.setAttribute = mockSetAttribute;
+}
 
 export class HtmlRewritingStream {
   constructor() {
